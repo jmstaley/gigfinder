9563e0c887c54528937555e9eda086fb3518fb14
Jon Staley <jon@spandexbob.co.uk>
2010-10-15 00:04:30 +0100

    tidy up some code

--- src/opt/gigfinder/gigfinder.py
+++ src/opt/gigfinder/gigfinder.py
@@ -31,6 +31,7 @@ __version__ = "0.0.1"
 # maybe do km to mile conversions
 
 class GigFinder:
+    """ main controller """
 
     def __init__(self):
         self.banner = None
@@ -41,7 +42,7 @@ class GigFinder:
         
         self.location = LocationUpdater()
         self.events = Events()
-        self.ui = gigfinder_ui.GigfinderUI(self.events.get_metros())
+        self.view = gigfinder_ui.GigfinderUI(self.events.get_metros())
         self.set_actions()
 
     def main(self):
@@ -49,45 +50,51 @@ class GigFinder:
         gtk.gdk.threads_enter()
         program = hildon.Program.get_instance()
         gtk.main()
-        gkt.gdk.threads_leave()
+        gtk.gdk.threads_leave()
 
     def set_actions(self):
-        self.ui.win.connect("destroy", self.quit, None)
-        self.ui.date_button.connect('value-changed',
+        """ setup the button actions """
+        self.view.win.connect("destroy", self.quit, None)
+        self.view.date_button.connect('value-changed',
                                     self.set_date,
                                     None)
-        self.ui.gps_toggle.connect('toggled', self.toggle_gps)
-        self.ui.gps_accuracy.connect('changed', self.set_accuracy)
-        self.ui.location_selector.connect('changed', self.set_location)
-        self.ui.update_button.connect('clicked',
+        self.view.gps_toggle.connect('toggled', self.toggle_gps)
+        self.view.gps_accuracy.connect('changed', self.set_accuracy)
+        self.view.location_selector.connect('changed', self.set_location)
+        self.view.update_button.connect('clicked',
                                       self.update,
                                       None)
-        self.ui.about_button.connect('clicked',
-                                     self.ui.about,
+        self.view.about_button.connect('clicked',
+                                     self.view.about,
                                      (self.version,
                                       self.authors,
                                       self.copyright))
         
     def set_location(self, selector, data):
+        """ set location to selected text """
         self.events.set_location(selector.get_current_text())
 
     def set_accuracy(self, selector, data):
+        """ set accuracy of gps search from selected text """
         self.location.set_accuracy(selector.get_current_text())
 
     def quit(self, widget, data):
+        """ Exit """
         self.location.stop(widget, data)
         thread.exit()
-        k.main_quit()
+        gtk.main_quit()
         return False
 
     def toggle_gps(self, gps_toggle):
-        self.ui.toggle_gps(gps_toggle)
+        """ toggle gps search """
+        self.view.toggle_gps(gps_toggle)
         if self.events.gps_search:
             self.events.set_gps_search(False)
         else:
             self.events.set_gps_search(True)
 
     def set_date(self, widget, data):
+        """ set search date """
         year, month, day = widget.get_date()
         self.events.set_date(date(year, month+1, day))
 
@@ -99,7 +106,7 @@ class GigFinder:
 
     def update_gigs(self):
         """ Get gig info """
-        gobject.idle_add(self.ui.show_message, "Getting events")
+        gobject.idle_add(self.view.show_message, "Getting events")
         
         if self.events.gps_search:
             if not 'applications' in os.path.abspath(__file__):
@@ -111,11 +118,11 @@ class GigFinder:
                 self.location.long = float(-0.082998)
          
         events = self.events.get_events()
-        gobject.idle_add(self.ui.show_events, 
+        gobject.idle_add(self.view.show_events, 
                          events, 
                          self.location, 
                          self.events.gps_search)
-        gobject.idle_add(self.ui.hide_message)
+        gobject.idle_add(self.view.hide_message)
         return True
 
 if __name__ == "__main__":
